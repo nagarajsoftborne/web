c6dde25f325c6bf51fe5aa55b5c6dba6132ba280
s4santhosh25 <s4santhosh25@gmail.com>
2018-04-12 09:44:00 +0530

    DatePicker function changed.

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import Layout from './Layout';
 import Register from './Register';
 import { Switch, Route } from 'react-router-dom';
 
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Switch>
-        <Route exact path='/' component={Layout}/>
-        <Route exact path='/register' component={Register}/>
-      </Switch>
+        <Switch>
+          <Route exact path='/' component={Layout}/>
+          <Route exact path='/register' component={Register}/>
+        </Switch>
       </div>
     );
   }
--- src/Register.js
+++ src/Register.js
@@ -3,6 +3,7 @@ import './Register.css';
 import PaperSimple from './component/Paper';
 import TextField from 'material-ui/TextField';
 import DatePicker from 'material-ui/DatePicker';
+import FlatButton from 'material-ui/FlatButton';
 
 const style = {
     height: 250,
@@ -17,21 +18,37 @@ class Register extends Component{
   constructor(props){
     super(props);
     this.state={
-        date: ''
+        date: '',
+        name: ''
     };
   }
 
-    date(e){
-        console.log(this.date);
+    registerData(e){
+            this.setState({
+                [e.target.name] : e.target.value
+            });
     }
 
+    DatePicker(e,data){
+        // this.setState({
+        //     date : '12323'
+        // });
+        console.log(data);
+    }
+
+    submitRegister(){
+        console.log(this.state);
+    }
+    
     render(){
         return(
             <PaperSimple circle={false} style={style}>
                 <div style={{color : '#808080', fontSize : 20}}><b>Register</b></div>
-                <TextField className="" fullWidth={false} hintText="Name" name="name" onChange={e=> this.submitLogin(e)} floatingLabelText="Name"/>
-                <DatePicker hintText="DOB" name="date" value={this.state.date} onChange={e => this.date(e)}/>
-          </PaperSimple>
+                <TextField className="" fullWidth={false} hintText="Name" name="name" onChange={e=> this.registerData(e)} floatingLabelText="Name"/>
+                <DatePicker floatingLabelText="DOB" hintText="DOB" name="date"  onChange={this.DatePicker}/>
+                <FlatButton label="Cancel" secondary={true} backgroundColor={'#E7E7E7'} hoverColor='#B39DDB' />&nbsp;
+                <FlatButton label="Submit" primary={true} backgroundColor={'#E7E7E7'} hoverColor='#B39DDB' onClick={e=> this.submitRegister(e)} />
+            </PaperSimple>
         )
     }
 }
